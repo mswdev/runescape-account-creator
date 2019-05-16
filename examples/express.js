a2210557a00cc1a9ab3df9a1aee25c482ecd0596
Kyle Stevenson <kyle@kylestevenson.me>
2019-05-16 00:02:47 -0700

    Update express example

--- examples/express.js
+++ examples/express.js
@@ -3,15 +3,18 @@ require('dotenv').config()
 // $ npm i express
 const express = require('express')
 
-const { configFromEnv, createAccount } = require('./index')
+const { buildAccountCreator } = require('../index')
 
 const app = express()
 
-const factoryConfig = configFromEnv()
+const accountCreator = buildAccountCreator('TWO_CAPTCHA_API_KEY')
 
 app.post('/create-account', async (req, resp) => {
   try {
-    const acc = await createAccount(factoryConfig)
+    const acc = await accountCreator.register({
+      email: req.body.email,
+      password: req.body.password
+    })
     resp.send(acc)
   } catch (e) {
     resp.send({ error: e.message }).status(500)
